a1eda17c34bcdd5b9297e9432b38bb1e03a8cecd
Muhamamd Bilal Qureshi <muhammad-bilal.qureshi@alten.com>
2025-02-03 15:11:16 +0100

    Lists

---
+++ Sequence/lists_intro.py
@@ -0,0 +1,23 @@
+computer_parts = ["computer",
+                  "monitor",
+                  "keyboard",
+                  "mouse",
+                  "mouse pad"]
+
+# for parts in computer_parts:
+#     print(parts)
+#
+# print()
+# print(computer_parts[2])
+#
+# print()
+#
+# print(computer_parts[0:3])
+# print(computer_parts[-1])
+print(computer_parts)
+
+# computer_parts[3] = "trackball"
+print(computer_parts[3:])
+
+computer_parts[3:] = ["trackball"]
+print(computer_parts)
