6f311077bc8dba3583f8345f77876e2f31968b10
Muhamamd Bilal Qureshi <muhammad-bilal.qureshi@alten.com>
2025-02-04 16:16:31 +0100

    The 'join' method

---
+++ Sequence/moreprint.py
@@ -0,0 +1,5 @@
+name = "Bilal"
+age = 31
+
+print(name, age, "Python", 2025)
+print(name, age, "Python", 2025, sep=", ")
--- Sequence/number_lists.py
+++ Sequence/number_lists.py
@@ -11,19 +11,26 @@ odd = [1, 3, 5, 7, 9]
 # print(even)
 # print(another_even)
 
-numbers = even + odd
+numbers = [even, odd]
 print(numbers)
 
-sorted_numbers = sorted(numbers)
-print(sorted_numbers)
-print(numbers)
+# nested lists
+for number_list in numbers:
+    print(number_list)
 
-digits = list("432985617")
-print(digits)
+    for value in number_list:
+        print(value)
 
-# more_numbers = list(numbers)
-# more_numbers = numbers[:]  # Copying a list - Python 2
-more_numbers = numbers.copy()
-print(more_numbers)
-print(numbers is more_numbers)  # Not equal lists - False
-print(numbers == more_numbers)  # Equal numbers of items in a list - True
+# sorted_numbers = sorted(numbers)
+# print(sorted_numbers)
+# print(numbers)
+#
+# digits = list("432985617")
+# print(digits)
+#
+# # more_numbers = list(numbers)
+# # more_numbers = numbers[:]  # Copying a list - Python 2
+# more_numbers = numbers.copy()
+# print(more_numbers)
+# print(numbers is more_numbers)  # Not equal lists - False
+# print(numbers == more_numbers)  # Equal numbers of items in a list - True
