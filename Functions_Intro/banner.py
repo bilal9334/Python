1fc96cedd94a3df552fbf16a938a8b59855ec3c2
Muhamamd Bilal Qureshi <muhammad-bilal.qureshi@alten.com>
2025-02-07 11:48:48 +0100

    Default parameters, Keyword Arguemnts, Docstring

---
+++ Functions_Intro/banner.py
@@ -0,0 +1,25 @@
+def banner_text(text="", width=80):
+    if len(text) > width - 4:
+        raise ValueError("String {0} is larger then specified width {1}."
+                         .format(text, width))
+
+    if text == "*":
+        print("*" * width)
+    else:
+        centered_text = text.center(width - 4)
+        output_string = "**{0}**".format(centered_text)
+        print(output_string)
+
+
+banner_text("*")
+banner_text("Always look on the bright side of life...")
+banner_text("If life seems jolly rotten,")
+banner_text("There's something you've forgotten!")
+banner_text("And that's to laugh and smile and dance and sing,", 80)
+banner_text(width=80)  # keyword argument - positional-or-keyword, either pass the arguments by position or name as
+# keyword
+banner_text("When you're feeling in the dumps,")
+banner_text("Don't be silly chumps,")
+banner_text("Just purse your lips and whistle - that's the thing!")
+banner_text("And...always look on the bright side of life...")
+banner_text("*")
--- Functions_Intro/functions.py
+++ Functions_Intro/functions.py
@@ -1,15 +1,46 @@
 def multiply(x, y):
+    """
+    Multiply 2 numbers
+
+    The function multiplies two integers, but you can also use it to
+    multiply a sequence. If you pass a string, for example, as the
+    first argument, you'll get the string repeated 'y' times as the
+    returned value.
+    :param x: The first integer passed as an argument from the function call
+    :param y: The second integer passed as an argument from the function call
+    :return: The answer of two integers multiplied
+    """
     result = x * y
     return result
 
 
 def is_palindrome(string):
+    """
+    Get a string from Standard Input (stdin)
+
+    A palindrome is a string that reads the same forwards as backwards.
+    :param string: The text that the user will see,
+        when prompted to enter a value.
+    :return: The 'text' that the user enters
+    """
     # backwards = string[::-1]
     # return backwards == string
     return string[::-1].casefold() == string.casefold()
 
 
 def palindrome_sentence(sentence):
+    """
+    Get a string from Standard Input (stdin)
+
+    The function checks if the 'sentence' entered is a palindrome and
+    loops through the sentence to check if a character is alphanumeric
+    which means it ignores whitespaces, capitalisation and punctuation in
+    the sentence.
+    :param sentence: The text that the user will see, when prompted to
+        enter a value.
+    :return: The 'is_palindrome' function is called to check if the 'string'
+        is a palindrome.
+    """
     string = ""
     for char in sentence:
         if char.isalnum():
